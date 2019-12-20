c928f957cc69a511b0e0f30281d905c6822d3c0c
Matthew Middleton <mattmidd6@gmail.com>
2019-12-20 14:15:06 -0500

    added stuff

--- uart.c
+++ uart.c
@@ -1,1 +1,33 @@
 
+#include "uart.h"
+
+//Baud Rate calculation:
+//SourceClk/(16*desiredBaud)
+//Example:
+//8000000/(16*115200) = 4.34
+//Fractional Part = 0.34 so UCBRSx = 0x4 according to Table 30-4. USBRSx Settings for Fractional Portion of N=fBRCLK/Baud Rate (Family User's Guide)
+//UCAxBRW = 4
+//UCBRFx = int( (4.34-4)*16) = 5
+// The steps here are from Section 30.3.10 of the Family User's Guide
+
+void begin(uint32_t baud, unsigned int srcClk, uint32_t srcClkHz)
+{
+    P5DIR = BIT1|BIT0;
+    PM5CTL0 &= ~LOCKPM5;
+
+    baudrate = baud;
+
+    initUART(baud, srcClk, srcClkHz);
+}
+
+void initUART(uint32_t baud, unsigned int srcClk, uint32_t srcClkHz)
+{
+
+}
+
+
+
+
+
+
+
--- uart.h
+++ uart.h
@@ -1,12 +1,16 @@
 #ifndef UART_H
 #define UART_H
 #include <msp430.h>
+#include <stdint.h>
 
+static uint32_t baudrate = 0;
 static volatile unsigned char *CSCTL0_H_ = CSCTL0_H;
 static volatile unsigned int *CSCTL1_ = CSCTL1;
 static volatile unsigned int *CSCTL2_ = CSCTL2;
 static volatile unsigned int *CSCTL3_ = CSCTL3;
 
+void begin(uint32_t baud, unsigned int srcClk, uint32_t srcClkHz);
 
+void initUART(uint32_t baud, unsigned int srcClk, uint32_t srcClkHz);
 
 #endif
