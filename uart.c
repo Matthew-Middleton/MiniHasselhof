ae6dd57ca6a60d666743de0fc2563786127f8d1e
Matthew Middleton <mattmidd6@gmail.com>
2019-12-21 12:39:57 -0500

    added register access for eusci uart. placed all register in structs for locality. began initialization for uart

--- uart.c
+++ uart.c
@@ -10,7 +10,7 @@
 //UCBRFx = int( (4.34-4)*16) = 5
 // The steps here are from Section 30.3.10 of the Family User's Guide
 
-void begin(uint32_t baud, unsigned int srcClk, uint32_t srcClkHz)
+void beginInit(uint32_t baud, unsigned int srcClk, uint32_t srcClkHz)
 {
     P5DIR = BIT1|BIT0;
     PM5CTL0 &= ~LOCKPM5;
@@ -20,7 +20,25 @@ void begin(uint32_t baud, unsigned int srcClk, uint32_t srcClkHz)
     initUART(baud, srcClk, srcClkHz);
 }
 
-void initUART(uint32_t baud, unsigned int srcClk, uint32_t srcClkHz)
+static void initUART(uint32_t baud, unsigned int srcClk, uint32_t srcClkHz)
+{
+
+    UCA0BRW_Val = srcClkHz/(16*baud);
+    /*Sets eUSCI to use no parity, send LSB first, use 8-bits for the packet
+     * with one stop bit in UART mode. This will be done in asynchronous mode
+     * using SMCLK with interrupts disabled. All characters received set UCRXIFG
+     * with all frames as data using no breaks. Software resets are disabled.
+     */
+    *(UART.UCA0CTLW0_) = UCPEN_0 | UCMSB_0 | UC7BIT_0 | UCSPB_0 |
+            UCMODE_0 | UCSYNC_0 | UCSSEL_2 | UCRXEIE_0 | UCBRKIE_0 |
+            UCDORM_0 | UCTXADDR_0 | UCTXBRK_0 | UCSWRST_0;
+    //Sets glitch time to 2ns. Anything received that is below this glitch period
+    //is considered erroneous and discarded.
+    *(UART.UCA0CTLW1_) = UCGLIT_0;
+
+}
+
+static void setClk(unsigned int srcClk, uint32_t)
 {
 
 }
--- uart.h
+++ uart.h
@@ -4,13 +4,38 @@
 #include <stdint.h>
 
 static uint32_t baudrate = 0;
-static volatile unsigned char *CSCTL0_H_ = CSCTL0_H;
-static volatile unsigned int *CSCTL1_ = CSCTL1;
-static volatile unsigned int *CSCTL2_ = CSCTL2;
-static volatile unsigned int *CSCTL3_ = CSCTL3;
+static unsigned int UCA0BRW_Val = 0;
+/*------------------------------------------------------*/
+//Clock registers
+typedef static struct _CLKS
+{
+    volatile unsigned char *CSCTL0_H_ = CSCTL0_H;
+    volatile unsigned int *CSCTL1_ = CSCTL1;
+    volatile unsigned int *CSCTL2_ = CSCTL2;
+    volatile unsigned int *CSCTL3_ = CSCTL3;
+} CLKS;
 
-void begin(uint32_t baud, unsigned int srcClk, uint32_t srcClkHz);
+/*------------------------------------------------------*/
+//eUSCI UART registers
+typedef static struct _UART
+{
+    volatile unsigned int *UCA0CTLW0_ = (volatile unsigned int *) &UCA0CTLW0;
+    volatile unsigned int *UCA0CTLW1_ = (volatile unsigned int *) &UCA0CTLW1;
+    volatile unsigned int *UCA0BRW_ = (volatile unsigned int *) &UCA0BRW;
+    volatile unsigned int *UCA0MCTLW_ = (volatile unsigned int *) &UCA0MCTLW;
+    volatile unsigned int *UCA0STATW_ = (volatile unsigned int *) &UCA0STATW;
+    volatile unsigned int *UCA0RXBUF_ = (volatile unsigned int *) &UCA0RXBUF;
+    volatile unsigned int *UCA0TXBUF_ = (volatile unsigned int *) &UCA0TXBUF;
+    volatile unsigned int *UCA0IE_ = (volatile unsigned int *) &UCA0IE;
+    volatile unsigned int *UCA0IFG_ = (volatile unsigned int *) &UCA0IFG;
+    volatile unsigned int *UCA0IV_ = (volatile unsigned int *) &UCA0IV;
+} UART;
 
-void initUART(uint32_t baud, unsigned int srcClk, uint32_t srcClkHz);
+
+void beginInit(uint32_t baud, unsigned int srcClk, uint32_t srcClkHz);
+
+static void initUART(uint32_t baud, unsigned int srcClk, uint32_t srcClkHz);
+
+static void setClk(unsigned int srcClk, uint32_t srcClkHz);
 
 #endif
