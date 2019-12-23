4eafcceeac60165385db8b8a90f1c71f96e868d8
Matthew Middleton <mattmidd6@gmail.com>
2019-12-23 15:53:44 -0600

    added small changes for testing

--- main.c
+++ main.c
@@ -5,8 +5,14 @@
  */
 int main(void)
 {
+    unsigned int rate = 9600;
+
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
+	//Use the rate as baudrate sourcing SMCLK as the clock
+	//with a frequency of 1MHz
+	beginInit(rate, UCSSEL_2, 1000000);
+
 	PM5CTL0 &= ~LOCKLPM5;
 	return 0;
 }
