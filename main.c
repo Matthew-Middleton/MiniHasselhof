33ffb6aac4d7eba8db0631b0c309648e45b05b48
Matthew Middleton <mattmidd6@gmail.com>
2019-12-25 13:54:11 -0600

    added receive capabilities for eUSCI UART

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include "uart.h"
-
+#define MAXSIZE 8
 /**
  * main.c
  */
 int main(void)
 {
     unsigned int rate = 9600;
-
+    uint8_t c = 0;
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
 	//Use the rate as baudrate sourcing SMCLK as the clock
@@ -14,5 +14,19 @@ int main(void)
 	beginInit(rate, UCSSEL_2, 1000000);
 
 	PM5CTL0 &= ~LOCKLPM5;
+	//program loop
+	for(;;)
+	{
+	    //transmission has started
+	    while(isInProgress())
+	    {
+	        //start bit has been received and the receive buffer is full
+	        if(startRead() && isFull())
+	        {
+	            //read a character from the receive buffer and assign its bits to c
+	            read(&c);
+	        }
+	    }
+	}
 	return 0;
 }
