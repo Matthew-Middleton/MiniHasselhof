2663a97320a2b25735170895e42da21cf3d465eb
Matthew Middleton <mattmidd6@gmail.com>
2019-12-29 09:40:19 -0800

    changed

--- main.c
+++ main.c
@@ -1,32 +1,23 @@
 #include "uart.h"
-#define MAXSIZE 8
-/**
- * main.c
- */
+#define BUFFSIZE 64
+
 int main(void)
 {
+    uint8_t buff[BUFFSIZE];
     unsigned int rate = 9600;
     uint8_t c = 0;
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
 	//Use the rate as baudrate sourcing SMCLK as the clock
 	//with a frequency of 1MHz
-	beginInit(rate, UCSSEL_2, 1000000);
+	beginInit(rate, UCSSEL_2, 1000000, buff, BUFFSIZE);
 
 	PM5CTL0 &= ~LOCKLPM5;
 	//program loop
 	for(;;)
 	{
 	    //transmission has started
-	    while(isInProgress())
-	    {
-	        //start bit has been received and the receive buffer is full
-	        if(startRead() && isFull())
-	        {
-	            //read a character from the receive buffer and assign its bits to c
-	            read(&c);
-	        }
-	    }
+
 	}
 	return 0;
 }
